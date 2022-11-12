6f0a2986aef82b6ea57b71df641c05560f590f02
Robin Gingras <robin@robingingras.com>
2022-11-12 01:00:02 -0800

    Added buildroot submodule

--- build.sh
+++ build.sh
@@ -1,27 +1,4 @@
 #!/bin/sh
 
-BUILDROOT_VERSION="2022.11-rc1"
-BUILDROOT_ARCHIVE="buildroot-${BUILDROOT_VERSION}.tar.xz"
-BUILDROOT_SIGN="buildroot-${BUILDROOT_VERSION}.tar.xz.sign"
-BUILDROOT_SOURCE="https://buildroot.org/downloads/"
-
-if [ ! -f ".extracted" ]; then
-  echo "Downloading buildroot..."
-
-  wget "${BUILDROOT_SOURCE}${BUILDROOT_ARCHIVE}"
-  wget "${BUILDROOT_SOURCE}${BUILDROOT_SIGN}"
-
-  if ! cat $BUILDROOT_SIGN | grep -q "$(sha256sum $BUILDROOT_ARCHIVE)"; then
-    echo "Checksum failed!"
-    echo "Got $(sha256sum $BUILDROOT_ARCHIVE)"
-    exit 1
-  fi
-
-  echo "Extracting buildroot..."
-  tar xf "${BUILDROOT_ARCHIVE}"
-  ln -s buildroot-${BUILDROOT_VERSION} buildroot
-  touch .extracted
-fi
-
 cd buildroot
 make $@
